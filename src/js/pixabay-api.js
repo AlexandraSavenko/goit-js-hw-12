802535dd420eb414d40daa03e4ad7a6739c9c338
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-07-11 11:01:13 +0300

    async await added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,20 @@
-export function fetchParams(item) {
+export async function fetchParams(item) {
   const API_KEY = '44790874-b72b714502b79af1442269c5d';
-  return fetch(
+  const response = await fetch(
     `https://pixabay.com/api/?key=${API_KEY}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
+  );
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  const data = await response.json();
+  return data;
+
+  //   return fetch(
+  //     `https://pixabay.com/api/?key=${API_KEY}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true`
+  //   ).then(res => {
+  //     if (!res.ok) {
+  //       throw new Error(res.status);
+  //     }
+  //     return res.json();
+  //   });
 }
--- src/main.js
+++ src/main.js
@@ -9,18 +9,24 @@ const formSearch = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery-container');
 
 formSearch.addEventListener('submit', getPicturesByValue);
-function getPicturesByValue(evt) {
+async function getPicturesByValue(evt) {
   evt.preventDefault();
   const form = evt.currentTarget;
-  const inputValue = form.elements.insert.value.toLowerCase().trim();
+  const inputValue = form.elements.inputtext.value.toLowerCase().trim();
   if (inputValue === '') {
     hideLoading();
     return errorParams();
   }
   showLoading();
   gallery.innerHTML = '';
-  fetchParams(inputValue)
-    .then(inputPictures)
-    .catch(errorParams)
-    .finally(() => form.reset());
+  try {
+    const picturesResponse = await fetchParams(inputValue);
+    inputPictures(picturesResponse);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // .then(inputPictures)
+  // .catch(errorParams)
+  // .finally(() => form.reset());
 }
