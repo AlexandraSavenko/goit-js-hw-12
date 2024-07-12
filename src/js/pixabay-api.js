8b58d8734c0402a60a3d6f5218cd95197822a26e
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-07-12 08:07:50 +0300

    pagination added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import iziToast from 'izitoast';
 
-export async function fetchParams(item) {
+export async function fetchParams(query, page) {
   let per_page = 15;
-  let page = 1;
   const params = {
     key: '44790874-b72b714502b79af1442269c5d',
-    q: item,
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
@@ -17,18 +15,6 @@ export async function fetchParams(item) {
     const response = await axios.get('https://pixabay.com/api/', { params });
     const data = response.data;
 
-    page += 1;
-    const loadingButton = document.querySelector('.btn-load');
-    if (page > 1) {
-      loadingButton.style.display = 'flex';
-    }
-    const totalResult = Math.ceil(data.total / per_page);
-    if (page > totalResult) {
-      return iziToast.error({
-        position: 'topRight',
-        message: "We're sorry, there are no more posts to load",
-      });
-    }
     return data;
   } catch (error) {
     throw new Error(error.response.status);
--- src/main.js
+++ src/main.js
@@ -5,31 +5,57 @@ import {
   errorParams,
 } from './js/render-functions';
 import { fetchParams } from './js/pixabay-api';
+
 const formSearch = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery-container');
 const loadingButton = document.querySelector('.btn-load');
 
+let currentQuery = '';
+let currentPage = 1;
+
 formSearch.addEventListener('submit', getPicturesByValue);
+loadingButton.addEventListener('click', getMorePictures);
+
 async function getPicturesByValue(evt) {
   evt.preventDefault();
   const form = evt.currentTarget;
   const inputValue = form.elements.inputtext.value.toLowerCase().trim();
+
   if (inputValue === '') {
     hideLoading();
     return errorParams();
   }
+
   showLoading();
   gallery.innerHTML = '';
+  currentQuery = inputValue;
+  currentPage = 1;
+
   try {
-    const picturesResponse = await fetchParams(inputValue);
+    const picturesResponse = await fetchParams(currentQuery, currentPage);
     inputPictures(picturesResponse);
+    loadingButton.style.display = 'flex';
   } catch (err) {
     errorParams();
   } finally {
     form.reset();
+    hideLoading();
   }
 }
-loadingButton.addEventListener('click', getMorePictures);
-function getMorePictures() {
-  console.log(loadingButton);
+async function getMorePictures() {
+  showLoading();
+  currentPage += 1;
+
+  try {
+    const picturesResponse = await fetchParams(currentQuery, currentPage);
+    inputPictures(picturesResponse);
+  } catch (err) {
+    errorParams();
+  } finally {
+    hideLoading();
+  }
 }
+// loadingButton.addEventListener('click', getMorePictures);
+// function getMorePictures() {
+//   console.log(loadingButton);
+// }
